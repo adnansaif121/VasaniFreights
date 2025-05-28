0c8927eea558526f26a263bdc6a7057f9198fe0e
Your Name <adnansaif121>
2025-05-28 21:18:52 +0530

    Export buttn added in Hare Krishna

--- components/HareKrishna.js
+++ components/HareKrishna.js
@@ -5,6 +5,8 @@ import { BellOutlined, UserOutlined, SearchOutlined, CloseOutlined, PlusOutlined
 import { getDatabase, ref, set, onValue, get, child } from "firebase/database";
 import ViewPartyDetails from './ViewHareKrishnaParty';
 import Highlighter from 'react-highlight-words';
+import * as XLSX from 'xlsx';
+import { saveAs } from 'file-saver';
 
 const bankData = [
     {
@@ -473,6 +475,22 @@ const HareKrishna = () => {
 
         getData();
     }, [refreshKey]);
+
+    const exportToExcel = () => {
+        // Prepare data: remove unwanted fields if needed
+        const exportData = displayDataSource.map(row => {
+            const { key, ...rest } = row; // remove key if you don't want it in Excel
+            return rest;
+        });
+
+        const worksheet = XLSX.utils.json_to_sheet(exportData);
+        const workbook = XLSX.utils.book_new();
+        XLSX.utils.book_append_sheet(workbook, worksheet, "Sheet1");
+        const excelBuffer = XLSX.write(workbook, { bookType: "xlsx", type: "array" });
+        const data = new Blob([excelBuffer], { type: "application/octet-stream" });
+        saveAs(data, "HareKrishna.xlsx");
+    };
+
 
     const handle_Search = (selectedKeys, confirm, dataIndex) => {
         confirm();
@@ -971,6 +989,11 @@ const HareKrishna = () => {
                             </Col>
                             <Col>
                                 <Button onClick={handleClearFilter}>Clear Filter</Button>
+                            </Col>
+                            <Col>
+                                <Button type="primary" style={{ marginLeft: '20px' }} onClick={exportToExcel}>
+                                    Export to Excel
+                                </Button>
                             </Col>
 
                         </Row>
